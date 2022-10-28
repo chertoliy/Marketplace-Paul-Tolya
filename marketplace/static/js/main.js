6bfcb5db9fd8a51c438fd6b32f4792e9bb356b27
Павел Смирнов <smirnov@transtelesoft.com>
2022-10-28 17:57:06 +0400

    Основа сервера (дополнение +)

---
+++ marketplace/static/js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+   $('.landing__slider').slick({
+      prevArrow: '<img class="landing__slider-arrow landing__slider-arrow-left" src="static/images/arrow-left.svg" alt="arrow-left">',
+      nextArrow: '<img class="landing__slider-arrow landing__slider-arrow-right" src="static/images/arrow-right.svg" alt="arrow-left">',
+      // infinite: true,
+      arrows: true,
+      dots: true,
+      // fade: true,
+      // autoplay: true,
+   });
+});
